1820865947ed6e45ca8326161f86c084c32757f0
harsh-k-117 <kulkarniharsh117@gmail.com>
2026-07-28 17:26:25 +0530

    demo

--- Arrays/plusOne.cpp
+++ Arrays/plusOne.cpp
@@ -10,7 +10,7 @@ Explanation: The array represents the integer 123.
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 
-Example 2:
+Example 
 Input: digits = [4,3,2,1]
 Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
