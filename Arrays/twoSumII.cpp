7107b019826ea3e6f81afb0cdb794413ee7daaa3
harsh-k-117 <kulkarniharsh117@gmail.com>
2026-08-07 20:11:32 +0530

    Revised solution for two sum II using two pointer

--- Arrays/twoSumII.cpp
+++ Arrays/twoSumII.cpp
@@ -33,21 +33,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size();
-        vector<int> ans = {};
         int left = 0;
         int right = n-1;
-
+        vector<int> ans = {};
         while(left<right){
-            int sum = numbers[left] + numbers[right];
-
-            if (sum == target){
+            if(numbers[left] + numbers[right] == target){
                 ans.push_back(left+1);
                 ans.push_back(right+1);
                 break;
-            }else if(sum < target){
-                left = left+1;
-            }else{
-                right = right-1;
+            }
+            else if(numbers[left] + numbers[right] < target){
+                left++;
+            }
+            else if(numbers[left] + numbers[right] > target){
+                right--;
             }
         }
         return ans;
